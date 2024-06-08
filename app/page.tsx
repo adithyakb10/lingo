78f1fb76b51fea7ca7b0f179e39199db65cd741c
Adithya K B <adithyakb93@gmail.com>
2024-06-08 11:03:21 +0530

    style: Add button library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello lingo!</h1>
+      <Button size="sm">Click Me!</Button>
     </div>
   );
 }
