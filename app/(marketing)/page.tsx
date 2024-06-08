44735db0f91b22d231acda0b1ddf07bff4f99b04
Adithya K B <adithyakb93@gmail.com>
2024-06-08 11:19:30 +0530

    style: Create marketing page skeleton

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello lingo!</h1>
-      <Button size="sm">Click Me!</Button>
-    </div>
-  );
+  return <div>This is the marketing page!</div>;
 }
