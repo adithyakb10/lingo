1a74f9e287dab9b20291d2b97531ea50bab5a96b
Adithya K B <adithyakb93@gmail.com>
2024-06-09 15:41:49 +0530

    style: Add footer, Complete landing/marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,11 +21,11 @@ export default function Home() {
           alt="Hero"
         />
       </div>
-      <div className="flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-8 items-center">
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn,practice and master new languages with Lingo
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,81 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+
 export default function Footer() {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2 ">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/hr.svg"
+            alt="Croatian"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/fr.svg"
+            alt="French"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/it.svg"
+            alt="Italian"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/jp.svg"
+            alt="Japanese"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/es.svg"
+            alt="Spanish"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+      </div>
     </footer>
   );
 }
