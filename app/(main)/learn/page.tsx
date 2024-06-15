1073110bedee69c1a23e9a55f3bc8c4069eddd4a
Adithya K B <adithyakb93@gmail.com>
2024-06-15 21:28:12 +0530

    feat : Add code for tracking user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,18 @@ import FeedWrapper from "@/components/FeedWrapper";
 import StickyWrapper from "@/components/StickyWrapper";
 import Header from "./Header";
 import UserProgress from "@/components/UserProgress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-export default function Learn() {
+export default async function Learn() {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
